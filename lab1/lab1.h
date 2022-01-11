0251c8538e0c8d435a1d17e6795ca3fc537cf15d
Satvick Acharya <satvickacharya16@gmail.com>
2022-01-11 15:29:40 -0500

    fixed "includes"

--- lab1/lab1.h
+++ lab1/lab1.h
@@ -4,3 +4,4 @@
 double split_bill(double base_amount, double tax_rate, double tip_rate, int num_people);
 double adjust_price(double original_price);
 int sandy_eats(char nemu_item[]);
+void imagine_fish(char thing[]);
